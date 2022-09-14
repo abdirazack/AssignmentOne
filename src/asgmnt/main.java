33c9ff5108f501a98d660cdb7d7530aa110d53c8
abdirizack <abdirizakomar254@gmail.com>
2022-09-14 16:29:04 +0300

    Changed amount due to auto generate itself

--- src/asgmnt/main.java
+++ src/asgmnt/main.java
@@ -2,6 +2,7 @@
  * 
  */
 package asgmnt;
+import java.time.Instant;
 import java.util.*;
 /**
  * @author Abdi
@@ -51,17 +52,21 @@ public class main {
             System.out.println("Enter item  price ");
             price[i] = scanner.nextDouble();
             
+            //Amount Due
+            double amountd = price[i]*quantity[i];
+            System.out.println("Amount Due: "+amountd);
+            AmountDue[i] = amountd;
+            
             //Amount Paid
             System.out.println("Enter Amount Paid ");
             AmountPaid[i] =scanner.nextDouble();
             
-            //Amount Due
-            System.out.println("Enter Amount Due ");
-            AmountDue[i] = scanner.nextDouble();
-            
             //Balance
             System.out.println("Enter Balance ");
             Balance[i] = scanner.nextDouble();
+            
+            //Date
+            System.out.println("Date: "+Instant.now());
         }
         
         //Getting totals
@@ -79,15 +84,15 @@ public class main {
         System.out.println("\t\t\t\tSales reciet");
         System.out.println("\t\t\t=======================");
         
-        System.out.println("item Number \t"+" Item name \t"+"Quantity \t"+" price \t \t"+" Amount Paid \t \t "+" Amount Due \t\t"+" Balance" );
-        System.out.println("--------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("item Number \t"+" Item name \t"+"Quantity \t"+" price \t \t"+" Amount Paid \t \t  "+" Amount Due \t\t"+" Balance" );
+        System.out.println("------------------------------------------------------------------------------------------------------------------------------");
         for(int i = 0; i < numberOfOrderedItems; i++){
            
-           System.out.println( itemsnumber[i]+"\t \t "+itemNames[i]+"\t \t "+quantity[i]+"\t \t "+price[i]+"\t \t "+AmountPaid[i]+"\t \t \t" +AmountDue[i]+"\t \t\t"+Balance[i]);
+           System.out.println( itemsnumber[i]+"\t \t "+itemNames[i]+"\t \t "+quantity[i]+"\t \t "+price[i]+"\t \t "+AmountPaid[i]+"\t \t \t \t" +AmountDue[i]+"\t \t\t"+Balance[i]);
         }
-        System.out.println("\t \t \t \t --------------------------------------------------------------------------------------------");
-        System.out.println("\t \t \t \t "+ tquantity +"\t \t \t\t "+ tamountpaid+ "\t\t\t"+ tamountDue+"\t\t\t"+tbalance);
-        System.out.println("\n \n");
+        System.out.println("\t \t \t \t -------------------------------------------------------------------------------------------------");
+        System.out.println("\t \t \t \t "+ tquantity +"\t \t \t\t "+ tamountpaid+ "\t\t\t\t"+ tamountDue+"\t\t\t"+tbalance);
+        System.out.println("\n");
         System.out.println("Sign: __________________________________");
         
         System.out.println("Waxaa Diyariyay Abdi Abdirizak Omar ID: CS0117001");
